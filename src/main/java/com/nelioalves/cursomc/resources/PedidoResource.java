d7488b85e424e52a52a999b419258dc9d6373453
Fábio Santos <fabio.sildossantos@gmail.com>
2019-03-08 06:31:16 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/nelioalves/cursomc/resources/PedidoResource.java
@@ -0,0 +1,27 @@
+package com.nelioalves.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.cursomc.domain.Pedido;
+import com.nelioalves.cursomc.services.PedidoService;
+
+@RestController // controlador rest
+@RequestMapping(value = "/pedidos") // end point rest de categorias
+public class PedidoResource {
+
+	@Autowired
+	private PedidoService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET) // get obtém dados
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Pedido obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+
+	}
+
+}
