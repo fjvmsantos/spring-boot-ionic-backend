282e99f4093dfb5008e7b9b956ebf608c8a5d7ff
Fábio Santos <fabio.sildossantos@gmail.com>
2019-06-14 07:15:52 -0300

    Refresh Token

--- src/main/java/com/nelioalves/cursomc/resources/AuthResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/AuthResource.java
@@ -12,20 +12,20 @@ import com.nelioalves.cursomc.security.JWTUtil;
 import com.nelioalves.cursomc.security.UserSS;
 import com.nelioalves.cursomc.services.UserService;
 
-//@RestController
-//@RequestMapping(value = "/auth")
+@RestController
+@RequestMapping(value="/auth")
 public class AuthResource {
-/*
+
 	@Autowired
 	private JWTUtil jwtUtil;
 	
-	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	@RequestMapping(value="/refresh_token", method=RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generationToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
 		return ResponseEntity.noContent().build();
 
-	}*/
+	}
 
 }
